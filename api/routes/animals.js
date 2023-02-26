840a4b00d8d65019980ac17d6a8ea96beaf16607
notPesol <pesol2012@hotmail.com>
2023-02-26 19:35:07 +0700

    Finish animals CRUD API

--- api/routes/animals.js
+++ api/routes/animals.js
@@ -6,7 +6,7 @@ const Animal = require("../models/Animal");
 router.get("/", async (req, res, next) => {
   const response = { ok: true };
   try {
-    const result = await Animal.find().populate({path: 'animalType'}).exec();
+    const result = await Animal.find().populate({ path: "animalType" }).exec();
     response.payload = result;
   } catch (error) {
     response.ok = false;
@@ -21,7 +21,7 @@ router.get("/:id", async (req, res, next) => {
   const response = { ok: true };
   try {
     const result = await Animal.findById(req.params.id)
-      .populate({path: 'animalType'})
+      .populate({ path: "animalType" })
       .exec();
     response.payload = result;
   } catch (error) {
@@ -40,6 +40,42 @@ router.post("/", async (req, res, next) => {
     const newAnimal = new Animal(req.body);
     await newAnimal.save();
     response.payload = newAnimal;
+  } catch (error) {
+    response.ok = false;
+    response.message = error.message;
+  }
+
+  res.json(response);
+});
+
+// Update Animal By Id
+router.put("/:id", async (req, res, next) => {
+  const response = { ok: true };
+  const {id} = req.params;
+  try {
+    const animal = await Animal.findByIdAndUpdate(id, req.body);
+    if (!animal) {
+      throw new Error(`Animal not found!, id: ${id}`)
+    }
+    response.payload = animal;
+  } catch (error) {
+    response.ok = false;
+    response.message = error.message;
+  }
+
+  res.json(response);
+});
+
+// Delete Animal By Id
+router.delete("/:id", async (req, res, next) => {
+  const response = { ok: true };
+  const {id} = req.params;
+  try {
+    const animal = await Animal.findByIdAndDelete(id, req.body);
+    if (!animal) {
+      throw new Error(`Animal not found!, id: ${id}`)
+    }
+    response.payload = animal;
   } catch (error) {
     response.ok = false;
     response.message = error.message;
